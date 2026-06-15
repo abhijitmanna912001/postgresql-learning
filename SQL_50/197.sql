cb0346c03b8c60941dda1e63772f275d1fff18e2
Abhijit Manna <abhijitmanna912001@gmail.com>
2026-06-15 21:43:20 +0530

    LeetCode 197

---
+++ SQL_50/197.sql
@@ -0,0 +1,10 @@
+-- Weather Table:
+-- id, recordDate, temperature
+-- Output -> id of higher temperature 
+SELECT
+    w1.id
+FROM
+    Weather w1
+    JOIN Weather w2 ON w1.recordDate = w2.recordDate + INTERVAL '1 day'
+WHERE
+    w1.temperature > w2.temperature;
